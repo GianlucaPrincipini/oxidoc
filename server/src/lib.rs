06a814e8ad8ea4b1dfe3551fc1884ad1e505baeb
Gianluca Principini <gianluca.principini@gmail.com>
2025-11-29 00:15:04 +0100

    Add core

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,14 +1,5 @@
-mod storage {
-    pub mod format {
-        pub mod kv {
-            pub mod collection;
-            pub mod db;
-        }
-    }
-    pub mod io {
-        pub mod writer;
-        pub mod reader;
-    }
+pub mod network {
+    pub mod listener;
 }
 pub mod cli {
     pub mod cli;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,8 @@
-use std::net::TcpListener;
-use std::io::{Read, Write};
-use clap::Parser;
+use server::network::listener::start_listener;
+
+const PORT: u16 = 7878;
+const ADDRESS: &str = "127.0.0.1";
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
-    println!("Server in ascolto su 127.0.0.1:7878");
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("Nuova connessione: {}", stream.peer_addr()?);
-                stream.write_all(b"Benvenuto su rusty_db!\n")?;
-                // Lettura opzionale dei dati inviati dal client
-                let mut buffer = [0; 512];
-                let _ = stream.read(&mut buffer)?;
-            }
-            Err(e) => {
-                eprintln!("Errore connessione: {}", e);
-            }
-        }
-    }
-    Ok(())
+    start_listener(ADDRESS, PORT)
 }
