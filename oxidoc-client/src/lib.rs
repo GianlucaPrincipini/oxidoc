5bb1447f80eb1ddefda58f5ef4d3b3e0218fcb70
Gianluca Principini <gianluca.principini@gmail.com>
2025-11-29 23:37:23 +0100

    clean client

--- oxidoc-client/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod utils {
-    pub mod args;
-    pub mod cli;
-}
-pub mod executor {
-    pub mod executor;
-}
--- oxidoc-client/src/main.rs
+++ oxidoc-client/src/main.rs
@@ -4,28 +4,4 @@ use oxidoc_client::executor::executor::CommandExecutorFactory;
 use oxidoc_client::utils::cli::Cli;
 
 fn main() {
-    loop {
-        print!("oxidoc> ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        if io::stdin().read_line(&mut input).is_err() {
-            println!("Errore nella lettura dell'input");
-            continue;
-        }
-        let input = input.trim();
-        if input.eq_ignore_ascii_case("exit") || input.eq_ignore_ascii_case("quit") {
-            println!("Uscita dal programma.");
-            break;
-        }
-        // Simula la suddivisione degli argomenti come da CLI
-        let args = std::iter::once("oxidoc").chain(input.split_whitespace());
-        match Cli::try_parse_from(args) {
-            Ok(cli) => {
-                let res = CommandExecutorFactory::get_executor(String::from("dummy")).execute(cli.command);
-                println!("{}", res);
-            }
-            ,
-            Err(e) => println!("Errore di parsing: {}", e),
-        }
-    }
 }
